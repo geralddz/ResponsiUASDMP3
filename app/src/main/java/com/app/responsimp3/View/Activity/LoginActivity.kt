29d599a4347b12b53dfe1145a5ce34196936009b
GeraldDZ <dzulfiqarg@gmail.com>
2022-07-16 10:40:16 +0700

    Initial commit

--- app/src/main/java/com/app/responsimp3/View/Activity/LoginActivity.kt
+++ app/src/main/java/com/app/responsimp3/View/Activity/LoginActivity.kt
@@ -3,11 +3,13 @@ package com.app.responsimp3.View.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.util.Patterns
+import android.widget.*
 import com.app.responsimp3.R
+import com.app.responsimp3.View.CustomDialog
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var btnLogin : Button
@@ -15,10 +17,11 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var etPassIn : EditText
     private lateinit var tvForgotPass : TextView
     private lateinit var tvregister : TextView
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        auth = FirebaseAuth.getInstance()
         btnLogin = findViewById(R.id.btnLogIn)
         etEmailIn = findViewById(R.id.etEmailLogIn)
         etPassIn = findViewById(R.id.etPasswordLogIn)
@@ -26,11 +29,61 @@ class LoginActivity : AppCompatActivity() {
         tvForgotPass = findViewById(R.id.tvForgotPass)
 
         tvregister.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
 
         tvForgotPass.setOnClickListener {
             startActivity(Intent(this, RecoveryActivity::class.java))
         }
+
+        btnLogin.setOnClickListener {
+            val email = etEmailIn.text.toString().trim()
+            val pass = etPassIn.text.toString().trim()
+            CustomDialog.showLoading(this)
+            if (CheckValidation(email, pass)){
+                LoginToServer(email,pass)
+            }
+        }
     }
+
+    private fun CheckValidation(email: String, pass: String): Boolean {
+        if(email.isEmpty()) {
+            etEmailIn.error = "Masukkan Email Anda"
+            etEmailIn.requestFocus()
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmailIn.error = "Masukkan Email dengan Benar"
+            etEmailIn.requestFocus()
+        } else if (pass.isEmpty()) {
+            etPassIn.error = "Masukkan Password Ada"
+            etPassIn.requestFocus()
+        } else{
+            return true
+        }
+        CustomDialog.hideLoading()
+        return false
+    }
+
+    private fun LoginToServer(email: String, pass: String) {
+        val credential = EmailAuthProvider.getCredential(email,pass)
+        firebaseAuth(credential)
+    }
+
+    private fun firebaseAuth(credential: AuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    CustomDialog.hideLoading()
+                    startActivity(Intent(this,HomeActivity::class.java))
+                    Toast.makeText(this, "Sign In Berhasil", Toast.LENGTH_SHORT).show()
+                    finishAffinity()
+                }else{
+                    Toast.makeText(this, "Sign In Gagal", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener {
+                CustomDialog.hideLoading()
+                Toast.makeText(this, "Pastikan Email dan Password Sudah Benar", Toast.LENGTH_SHORT).show()
+            }
+    }
+
 }
--- app/src/main/java/com/app/responsimp3/View/Activity/RecoveryActivity.kt
+++ app/src/main/java/com/app/responsimp3/View/Activity/RecoveryActivity.kt
@@ -1,12 +1,69 @@
 package com.app.responsimp3.View.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.app.responsimp3.R
+import com.google.firebase.auth.FirebaseAuth
 
 class RecoveryActivity : AppCompatActivity() {
+    private lateinit var toolbarreset: Toolbar
+    private lateinit var etEmailreset : EditText
+    private lateinit var btnSendMail : Button
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recovery)
+        initActionBar()
+        auth = FirebaseAuth.getInstance()
+        toolbarreset = findViewById(R.id.tbForgotPass)
+        etEmailreset = findViewById(R.id.etEmailResetPass)
+        btnSendMail = findViewById(R.id.btnSendEmail)
+        
+
+        toolbarreset.setNavigationOnClickListener {
+            finish()
+        }
+
+        btnSendMail.setOnClickListener {
+            val email = etEmailreset.text.toString().trim()
+            if(email.isEmpty()) {
+                etEmailreset.error = "Masukkan Email Anda"
+                etEmailreset.requestFocus()
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                etEmailreset.error = "Masukkan Email dengan Benar"
+                etEmailreset.requestFocus()
+                return@setOnClickListener
+            }else{
+                forgotpass(email)
+            }
+        }
+    }
+
+    private fun forgotpass(email: String) {
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    Toast.makeText(this, "Your reset password has been sent to your email", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finishAffinity()
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Pastikan Email Sudah Benar", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun initActionBar() {
+        setSupportActionBar(toolbarreset)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
     }
 }
